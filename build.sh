27ba456bfe15edfcd23ca10eb620c9a35377b0ee
Martin Islas <mislas87@gmail.com>
2020-04-04 12:19:41 -0600

    chore(build.sh): add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+IMAGE_NAME=oci-oke-cli
+IMAGE_TAG=0.1.0
+
+REPOSITORY_ID=$IMAGE_NAME:$IMAGE_TAG
+
+if [ -z $1 ];
+then
+	REPOSITORY_ID=$IMAGE_NAME:$IMAGE_TAG
+else
+	REPOSITORY_ID=$1/$IMAGE_NAME:$IMAGE_TAG
+fi
+
+docker build --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` \
+             --build-arg VCS_REF=`git rev-parse --short HEAD` \
+             --build-arg VERSION=$IMAGE_TAG \
+             -t $REPOSITORY_ID .
